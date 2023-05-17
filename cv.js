2e936f3563a8f9351aabf0caf5e4ca66babf6082
Archimonde32 <dkgaming718@gmail.com>
2023-05-17 11:58:58 +0200

    lightmodfin1

--- cv.js
+++ cv.js
@@ -1,7 +1,17 @@
 function myFunction() {
     var body = document.body;
+    var section = document.getElementById('masection');
+    var section2 = document.getElementById('masection2');
+    var section3 = document.getElementById('masection3');
+    var section4 = document.getElementById('masection4');
+    var section5 = document.getElementById('masection5');
        // toggle the theme
     body.classList.toggle("white");
+    section.classList.toggle("white");
+    section2.classList.toggle("white");
+    section3.classList.toggle("white");
+    section4.classList.toggle("white");
+    section5.classList.toggle("white");
     let button = document.getElementById('button');
     
        // change the button text
