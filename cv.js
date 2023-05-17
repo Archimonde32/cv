e174f890847e7f6a9b78406577d4192f0f2011cd
Archimonde32 <dkgaming718@gmail.com>
2023-05-17 10:15:28 +0200

    lightmod2

--- cv.js
+++ cv.js
@@ -1,15 +1,14 @@
 function myFunction() {
     var body = document.body;
-    
        // toggle the theme
     body.classList.toggle("white");
     let button = document.getElementById('button');
-
+    
        // change the button text
-    if (button.innerHTML == "White Mode") {
+    if (button.innerHTML == "Light Mode") {
         button.innerHTML = "Normal Mode";
     } else {
-        button.innerHTML = "White Mode"
+        button.innerHTML = "Light Mode"
     }
     }
     
