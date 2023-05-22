945731dab4c5b0351fca460e1efb6db7e46ac124
Archimonde32 <dkgaming718@gmail.com>
2023-05-22 15:57:39 +0200

    classchiant

--- cv.js
+++ cv.js
@@ -1,17 +1,11 @@
 function myFunction() {
     var body = document.body;
-    var section = document.getElementById('masection');
-    var section2 = document.getElementById('masection2');
-    var section3 = document.getElementById('masection3');
-    var section4 = document.getElementById('masection4');
-    var section5 = document.getElementById('masection5');
+    var sections = document.querySelectorAll('.masection');
        // toggle the theme
     body.classList.toggle("white");
-    section.classList.toggle("white");
-    section2.classList.toggle("white");
-    section3.classList.toggle("white");
-    section4.classList.toggle("white");
-    section5.classList.toggle("white");
+    sections.forEach((sections) => {
+        sections.classList.toggle("white");
+    });
     let button = document.getElementById('button');
 
        // change the button text
